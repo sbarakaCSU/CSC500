ade86ae4a6b35b024cee38516a0a6b69a7f4dfa0
sbarakaCSU <salem.baraka@csuglobal.edu>
2024-09-15 17:40:42 -0500

    Update CSC500_Module1_P1.py

--- Module1/CSC500_Module1_P1.py
+++ Module1/CSC500_Module1_P1.py
@@ -8,11 +8,3 @@ print ("The sum of the two numbers are: ", sum)
 #Calculating difference
 difference = num1 - num2
 print ("The difference of two numbers are: ", difference)
-
-#Calculating product
-product = num1 * num2
-print ("The product of two numbers are: ", product)
-
-#Calculating quotient
-quotient = num1 // num2
-print ("The quotient of two numbers are: ", quotient)
